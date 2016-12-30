b23548508af827d9e74ae7999653190afa7b12d1
obelisk <aaron.reyna@gmail.com>
2016-12-30 15:18:32 -0800

    added in patternFinder()

--- stock_machine_learn.py
+++ stock_machine_learn.py
@@ -5,19 +5,46 @@ import matplotlib.dates as mdates
 import numpy as np
 from matplotlib import style
 style.use("ggplot")
+import time
 print("libs imported")
 
+date, bid, ask = np.loadtxt('GBP/GBPUSD1d.txt', unpack=True,
+                            delimiter=',',
+                            converters={0: mdates.strpdate2num('%Y%m%d%H%M%S')})
 
 def percentchange(startPoint, currentPoint):
-    return ((currentPoint - startPoint)/startPoint)*100
+    return ((float(currentPoint) - startPoint)/startPoint)*100.00
 
+def patternFinder():
+    avgLine = ((bid+ask)/2)
+    x = len(avgLine)-30
+
+    y = 11
+    while y < x:
+        p1 = percentchange(avgLine[y-10], avgLine[y-9])
+        p2 = percentchange(avgLine[y - 10], avgLine[y - 8])
+        p3 = percentchange(avgLine[y - 10], avgLine[y - 7])
+        p4 = percentchange(avgLine[y - 10], avgLine[y - 6])
+        p5 = percentchange(avgLine[y - 10], avgLine[y - 5])
+        p6 = percentchange(avgLine[y - 10], avgLine[y - 4])
+        p7 = percentchange(avgLine[y - 10], avgLine[y - 3])
+        p8 = percentchange(avgLine[y - 10], avgLine[y - 2])
+        p9 = percentchange(avgLine[y - 10], avgLine[y - 1])
+        p10 = percentchange(avgLine[y - 10], avgLine[y])
+
+        outcomeRange = avgLine[y+20:y+30]
+        currentPoint = avgLine[y]
+
+        print reduce(lambda x, y: x+y, outcomeRange) / len(outcomeRange)
+        print currentPoint
+        print p1, p2, p3, p4, p5, p6, p7, p8, p9, p10
+        y += 1
+        time.sleep(5555)
 
 
 
 def graphRawFX():
-    date,bid,ask = np.loadtxt('GBP/GBPUSD1d.txt', unpack=True,
-                              delimiter=',',
-                              converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
+
 
     fig=plt.figure(figsize=(10,7))
     ax1 = plt.subplot2grid(((40,40)), (0,0), rowspan=40, colspan=40)
@@ -35,5 +62,5 @@ def graphRawFX():
 
 
 
-
-graphRawFX()
+patternFinder()
+#graphRawFX()
