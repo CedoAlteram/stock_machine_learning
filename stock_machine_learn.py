c571a82c4df0d9977205b4e7fc0b0f9780f77c4c
obelisk <aaron.reyna@gmail.com>
2016-12-30 14:56:52 -0800

    added in percent change function

--- stock_machine_learn.py
+++ stock_machine_learn.py
@@ -5,9 +5,14 @@ import matplotlib.dates as mdates
 import numpy as np
 from matplotlib import style
 style.use("ggplot")
-
-
 print("libs imported")
+
+
+def percentchange(startPoint, currentPoint):
+    return ((currentPoint - startPoint)/startPoint)*100
+
+
+
 
 def graphRawFX():
     date,bid,ask = np.loadtxt('GBP/GBPUSD1d.txt', unpack=True,
@@ -15,23 +20,20 @@ def graphRawFX():
                               converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
 
     fig=plt.figure(figsize=(10,7))
-
     ax1 = plt.subplot2grid(((40,40)), (0,0), rowspan=40, colspan=40)
     ax1.plot(date,bid)
     ax1.plot(date,ask)
     plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
-
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
-
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(45)
     ax1_2 = ax1.twinx()
     ax1_2.fill_between(date, 0, (ask-bid), facecolor='g', alpha=.3)
-
-
     plt.subplots_adjust(bottom=.23)
-
     plt.grid(True)
     plt.show()
+
+
+
 
 graphRawFX()
