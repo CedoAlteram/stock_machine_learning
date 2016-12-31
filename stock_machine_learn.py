d13ae1293d57243d4370f6998aeeb29faca9becd
obelisk <aaron.reyna@gmail.com>
2016-12-30 17:32:53 -0800

    plotted all plot togther

--- stock_machine_learn.py
+++ stock_machine_learn.py
@@ -14,32 +14,59 @@ date, bid, ask = np.loadtxt('GBP/GBPUSD1d.txt', unpack=True,
                             delimiter=',',
                             converters={0: mdates.strpdate2num('%Y%m%d%H%M%S')})
 
-avgLine = ((bid+ask)/2)
-patternAr = []
-performanceAr = []
-patForRec = []
+
+
 
 def percentchange(startPoint, currentPoint):
-    return ((float(currentPoint) - startPoint)/abs(startPoint))*100.00
+    try:
+        x = ((float(currentPoint) - startPoint)/abs(startPoint))*100.00
+        if x == 0.0:
+            return 0.00000000001
+        else:
+            return x
+    except:
+        return 0.000000001
 
 def patternStorage():
     patStartTime = time.time()
 
-    x = len(avgLine)-30
+    x = len(avgLine)-60
 
-    y = 11
+    y = 31
     while y < x:
         pattern = []
-        p1 = percentchange(avgLine[y-10], avgLine[y-9])
-        p2 = percentchange(avgLine[y - 10], avgLine[y - 8])
-        p3 = percentchange(avgLine[y - 10], avgLine[y - 7])
-        p4 = percentchange(avgLine[y - 10], avgLine[y - 6])
-        p5 = percentchange(avgLine[y - 10], avgLine[y - 5])
-        p6 = percentchange(avgLine[y - 10], avgLine[y - 4])
-        p7 = percentchange(avgLine[y - 10], avgLine[y - 3])
-        p8 = percentchange(avgLine[y - 10], avgLine[y - 2])
-        p9 = percentchange(avgLine[y - 10], avgLine[y - 1])
-        p10 = percentchange(avgLine[y - 10], avgLine[y])
+        p1 = percentchange(avgLine[y - 30], avgLine[y - 29])
+        p2 = percentchange(avgLine[y - 30], avgLine[y - 28])
+        p3 = percentchange(avgLine[y - 30], avgLine[y - 27])
+        p4 = percentchange(avgLine[y - 30], avgLine[y - 26])
+        p5 = percentchange(avgLine[y - 30], avgLine[y - 25])
+        p6 = percentchange(avgLine[y - 30], avgLine[y - 24])
+        p7 = percentchange(avgLine[y - 30], avgLine[y - 23])
+        p8 = percentchange(avgLine[y - 30], avgLine[y - 22])
+        p9 = percentchange(avgLine[y - 30], avgLine[y - 21])
+        p10 = percentchange(avgLine[y - 30], avgLine[y - 20])
+        p11 = percentchange(avgLine[y - 30], avgLine[y - 19])
+        p12 = percentchange(avgLine[y - 30], avgLine[y - 18])
+        p13 = percentchange(avgLine[y - 30], avgLine[y - 17])
+        p14 = percentchange(avgLine[y - 30], avgLine[y - 16])
+        p15 = percentchange(avgLine[y - 30], avgLine[y - 15])
+        p16 = percentchange(avgLine[y - 30], avgLine[y - 14])
+        p17 = percentchange(avgLine[y - 30], avgLine[y - 13])
+        p18 = percentchange(avgLine[y - 30], avgLine[y - 12])
+        p19 = percentchange(avgLine[y - 30], avgLine[y - 11])
+        p20 = percentchange(avgLine[y - 30], avgLine[y - 10])
+        p21 = percentchange(avgLine[y - 30], avgLine[y - 9])
+        p22 = percentchange(avgLine[y - 30], avgLine[y - 8])
+        p23 = percentchange(avgLine[y - 30], avgLine[y - 7])
+        p24 = percentchange(avgLine[y - 30], avgLine[y - 6])
+        p25 = percentchange(avgLine[y - 30], avgLine[y - 5])
+        p26 = percentchange(avgLine[y - 30], avgLine[y - 4])
+        p27 = percentchange(avgLine[y - 30], avgLine[y - 3])
+        p28 = percentchange(avgLine[y - 30], avgLine[y - 2])
+        p29 = percentchange(avgLine[y - 30], avgLine[y - 1])
+        p30 = percentchange(avgLine[y - 30], avgLine[y])
+
+
 
         outcomeRange = avgLine[y+20:y+30]
         currentPoint = avgLine[y]
@@ -60,6 +87,26 @@ def patternStorage():
         pattern.append(p8)
         pattern.append(p9)
         pattern.append(p10)
+        pattern.append(p11)
+        pattern.append(p12)
+        pattern.append(p13)
+        pattern.append(p14)
+        pattern.append(p15)
+        pattern.append(p16)
+        pattern.append(p17)
+        pattern.append(p18)
+        pattern.append(p19)
+        pattern.append(p20)
+        pattern.append(p21)
+        pattern.append(p22)
+        pattern.append(p23)
+        pattern.append(p24)
+        pattern.append(p25)
+        pattern.append(p26)
+        pattern.append(p27)
+        pattern.append(p28)
+        pattern.append(p29)
+        pattern.append(p30)
 
         patternAr.append(pattern)
         performanceAr.append(futureOutcome)
@@ -74,16 +121,36 @@ def patternStorage():
 def currentPattern():
 
 
-    cp1 = percentchange(avgLine[-11],avgLine[-10])
-    cp2 = percentchange(avgLine[-11], avgLine[-9])
-    cp3 = percentchange(avgLine[-11], avgLine[-8])
-    cp4 = percentchange(avgLine[-11], avgLine[-7])
-    cp5 = percentchange(avgLine[-11], avgLine[-6])
-    cp6 = percentchange(avgLine[-11], avgLine[-5])
-    cp7 = percentchange(avgLine[-11], avgLine[-4])
-    cp8 = percentchange(avgLine[-11], avgLine[-3])
-    cp9 = percentchange(avgLine[-11], avgLine[-2])
-    cp10 = percentchange(avgLine[-11], avgLine[-1])
+    cp1 = percentchange(avgLine[-31],avgLine[-30])
+    cp2 = percentchange(avgLine[-31], avgLine[-29])
+    cp3 = percentchange(avgLine[-31], avgLine[-28])
+    cp4 = percentchange(avgLine[-31], avgLine[-27])
+    cp5 = percentchange(avgLine[-31], avgLine[-26])
+    cp6 = percentchange(avgLine[-31], avgLine[-25])
+    cp7 = percentchange(avgLine[-31], avgLine[-24])
+    cp8 = percentchange(avgLine[-31], avgLine[-23])
+    cp9 = percentchange(avgLine[-31], avgLine[-22])
+    cp10 = percentchange(avgLine[-31], avgLine[-21])
+    cp11 = percentchange(avgLine[-31],avgLine[-20])
+    cp12 = percentchange(avgLine[-31], avgLine[-19])
+    cp13 = percentchange(avgLine[-31], avgLine[-18])
+    cp14 = percentchange(avgLine[-31], avgLine[-17])
+    cp15 = percentchange(avgLine[-31], avgLine[-16])
+    cp16 = percentchange(avgLine[-31], avgLine[-15])
+    cp17 = percentchange(avgLine[-31], avgLine[-14])
+    cp18 = percentchange(avgLine[-31], avgLine[-13])
+    cp19 = percentchange(avgLine[-31], avgLine[-12])
+    cp20 = percentchange(avgLine[-31], avgLine[-11])
+    cp21 = percentchange(avgLine[-31],avgLine[-10])
+    cp22 = percentchange(avgLine[-31], avgLine[-9])
+    cp23 = percentchange(avgLine[-31], avgLine[-8])
+    cp24 = percentchange(avgLine[-31], avgLine[-7])
+    cp25 = percentchange(avgLine[-31], avgLine[-6])
+    cp26 = percentchange(avgLine[-31], avgLine[-5])
+    cp27 = percentchange(avgLine[-31], avgLine[-4])
+    cp28 = percentchange(avgLine[-31], avgLine[-3])
+    cp29 = percentchange(avgLine[-31], avgLine[-2])
+    cp30 = percentchange(avgLine[-31], avgLine[-1])
 
     patForRec.append(cp1)
     patForRec.append(cp2)
@@ -95,10 +162,35 @@ def currentPattern():
     patForRec.append(cp8)
     patForRec.append(cp9)
     patForRec.append(cp10)
+    patForRec.append(cp11)
+    patForRec.append(cp12)
+    patForRec.append(cp13)
+    patForRec.append(cp14)
+    patForRec.append(cp15)
+    patForRec.append(cp16)
+    patForRec.append(cp17)
+    patForRec.append(cp18)
+    patForRec.append(cp19)
+    patForRec.append(cp20)
+    patForRec.append(cp21)
+    patForRec.append(cp22)
+    patForRec.append(cp23)
+    patForRec.append(cp24)
+    patForRec.append(cp25)
+    patForRec.append(cp26)
+    patForRec.append(cp27)
+    patForRec.append(cp28)
+    patForRec.append(cp29)
+    patForRec.append(cp30)
 
     print patForRec
 
 def patternRecognition():
+
+    patFound = 0
+    plotPatAr = []
+
+
     for eachPattern in patternAr:
         sim1 = 100.00 - abs(percentchange(eachPattern[0], patForRec[0]))
         sim2 = 100.00 - abs(percentchange(eachPattern[1], patForRec[1]))
@@ -111,12 +203,38 @@ def patternRecognition():
         sim9 = 100.00 - abs(percentchange(eachPattern[8], patForRec[8]))
         sim10 = 100.00 - abs(percentchange(eachPattern[9], patForRec[9]))
 
-        howSim = (sim1+sim2+sim3+sim4+sim5+sim6+sim7+sim8+sim9+sim10)/10.00
+        sim11 = 100.00 - abs(percentchange(eachPattern[10], patForRec[10]))
+        sim12 = 100.00 - abs(percentchange(eachPattern[11], patForRec[11]))
+        sim13 = 100.00 - abs(percentchange(eachPattern[12], patForRec[12]))
+        sim14 = 100.00 - abs(percentchange(eachPattern[13], patForRec[13]))
+        sim15 = 100.00 - abs(percentchange(eachPattern[14], patForRec[14]))
+        sim16 = 100.00 - abs(percentchange(eachPattern[15], patForRec[15]))
+        sim17 = 100.00 - abs(percentchange(eachPattern[16], patForRec[16]))
+        sim18 = 100.00 - abs(percentchange(eachPattern[17], patForRec[17]))
+        sim19 = 100.00 - abs(percentchange(eachPattern[18], patForRec[18]))
+        sim20 = 100.00 - abs(percentchange(eachPattern[19], patForRec[19]))
+        sim21 = 100.00 - abs(percentchange(eachPattern[20], patForRec[20]))
 
-        if howSim > 70:
+        sim22 = 100.00 - abs(percentchange(eachPattern[21], patForRec[21]))
+        sim23 = 100.00 - abs(percentchange(eachPattern[22], patForRec[22]))
+        sim24 = 100.00 - abs(percentchange(eachPattern[23], patForRec[23]))
+        sim25 = 100.00 - abs(percentchange(eachPattern[24], patForRec[24]))
+        sim26 = 100.00 - abs(percentchange(eachPattern[25], patForRec[25]))
+        sim27 = 100.00 - abs(percentchange(eachPattern[26], patForRec[26]))
+        sim28 = 100.00 - abs(percentchange(eachPattern[27], patForRec[27]))
+        sim29 = 100.00 - abs(percentchange(eachPattern[28], patForRec[28]))
+        sim30 = 100.00 - abs(percentchange(eachPattern[29], patForRec[29]))
+
+        howSim = (sim1 + sim2 + sim3 + sim4 + sim5 + sim6 + sim7 + sim8 + sim9 + sim10 +
+                  sim11 + sim12 + sim13 + sim14 + sim15 + sim16 + sim17 + sim18 + sim19 + sim20 +
+                  sim21 + sim22 + sim23 + sim24 + sim25 + sim26 + sim27 + sim28 + sim29 + sim30)/30.00
+
+        if howSim > 75:
             patdex = patternAr.index(eachPattern)
 
-            print '##################################'
+            patFound = 1
+
+            '''print '##################################'
             print '##################################'
 
             print patForRec
@@ -126,15 +244,28 @@ def patternRecognition():
 
             print eachPattern
             print '----------------------------------'
-            print 'predicted outcome', performanceAr[patdex]
-            xp = [1,2,3,4,5,6,7,8,9,10]
-            fig = plt.figure()
-            plt.plot(xp,patForRec)
-            plt.plot(xp, eachPattern)
-            plt.show()
+            print 'predicted outcome', performanceAr[patdex]'''
 
-            print '##################################'
-            print '##################################'
+
+            #xp = [:31]
+            xp = [1,2,3,4,5,6,7,8,9,10,
+                  11,12,13,14,15,16,17,18,19,20,
+                  21,22,23,24,25,26,27,28,29,30]
+
+            plotPatAr.append(eachPattern)
+
+    if patFound == 1:
+
+        fig = plt.figure(figsize=(10,6))
+
+        for eachPatt in plotPatAr:
+            plt.plot(xp, eachPatt)
+
+
+        plt.plot(xp,patForRec, '#54fff7', linewidth = 3)
+        plt.grid(True)
+        plt.title('Pattern Recognition')
+        plt.show()
 
 
 
@@ -157,15 +288,23 @@ def graphRawFX():
     plt.grid(True)
     plt.show()
 
+dataLength = int(bid.shape[0])
+print 'data length is', dataLength
 
-patternStorage()
-currentPattern()
-patternRecognition()
-totalTime = time.time() - totalStart
-print 'Entire processing time took:', totalTime, 'seconds'
+toWhat = 37000
+while toWhat < dataLength:
 
-#currentPattern()
-#print percentchange(-10,10)
-#print patternAr[5]
-#patternStorage()
-#graphRawFX()
+    avgLine = ((bid+ask)/2)
+    avgLine = avgLine[:toWhat]
+    patternAr = []
+    performanceAr = []
+    patForRec = []
+
+
+    patternStorage()
+    currentPattern()
+    patternRecognition()
+    totalTime = time.time() - totalStart
+    print 'Entire processing time took:', totalTime, 'seconds'
+    moveOn = raw_input('press ENTER to continue...')
+    toWhat += 1
