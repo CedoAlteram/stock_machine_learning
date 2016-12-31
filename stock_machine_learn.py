b8f862a746c55c95bbf5ac51df17eaca8ff9af09
obelisk <aaron.reyna@gmail.com>
2016-12-30 16:29:00 -0800

    multiple changes and plotting of the similar data sets

--- stock_machine_learn.py
+++ stock_machine_learn.py
@@ -8,19 +8,28 @@ style.use("ggplot")
 import time
 print("libs imported")
 
+totalStart = time.time()
+
 date, bid, ask = np.loadtxt('GBP/GBPUSD1d.txt', unpack=True,
                             delimiter=',',
                             converters={0: mdates.strpdate2num('%Y%m%d%H%M%S')})
 
-def percentchange(startPoint, currentPoint):
-    return ((float(currentPoint) - startPoint)/startPoint)*100.00
+avgLine = ((bid+ask)/2)
+patternAr = []
+performanceAr = []
+patForRec = []
 
-def patternFinder():
-    avgLine = ((bid+ask)/2)
+def percentchange(startPoint, currentPoint):
+    return ((float(currentPoint) - startPoint)/abs(startPoint))*100.00
+
+def patternStorage():
+    patStartTime = time.time()
+
     x = len(avgLine)-30
 
     y = 11
     while y < x:
+        pattern = []
         p1 = percentchange(avgLine[y-10], avgLine[y-9])
         p2 = percentchange(avgLine[y - 10], avgLine[y - 8])
         p3 = percentchange(avgLine[y - 10], avgLine[y - 7])
@@ -34,12 +43,100 @@ def patternFinder():
 
         outcomeRange = avgLine[y+20:y+30]
         currentPoint = avgLine[y]
+        try:
+            avgOutcome = reduce(lambda x, y: x+y, outcomeRange) / len(outcomeRange)
+        except Exception, e:
+            print str(e)
+            avgOutcome=0
 
-        print reduce(lambda x, y: x+y, outcomeRange) / len(outcomeRange)
-        print currentPoint
-        print p1, p2, p3, p4, p5, p6, p7, p8, p9, p10
+        futureOutcome = percentchange(currentPoint, avgOutcome)
+        pattern.append(p1)
+        pattern.append(p2)
+        pattern.append(p3)
+        pattern.append(p4)
+        pattern.append(p5)
+        pattern.append(p6)
+        pattern.append(p7)
+        pattern.append(p8)
+        pattern.append(p9)
+        pattern.append(p10)
+
+        patternAr.append(pattern)
+        performanceAr.append(futureOutcome)
+
         y += 1
-        time.sleep(5555)
+
+    patEndTime = time.time()
+    print len(patternAr)
+    print len(performanceAr)
+    print 'Pattern storage took:', patEndTime - patStartTime, 'seconds'
+
+def currentPattern():
+
+
+    cp1 = percentchange(avgLine[-11],avgLine[-10])
+    cp2 = percentchange(avgLine[-11], avgLine[-9])
+    cp3 = percentchange(avgLine[-11], avgLine[-8])
+    cp4 = percentchange(avgLine[-11], avgLine[-7])
+    cp5 = percentchange(avgLine[-11], avgLine[-6])
+    cp6 = percentchange(avgLine[-11], avgLine[-5])
+    cp7 = percentchange(avgLine[-11], avgLine[-4])
+    cp8 = percentchange(avgLine[-11], avgLine[-3])
+    cp9 = percentchange(avgLine[-11], avgLine[-2])
+    cp10 = percentchange(avgLine[-11], avgLine[-1])
+
+    patForRec.append(cp1)
+    patForRec.append(cp2)
+    patForRec.append(cp3)
+    patForRec.append(cp4)
+    patForRec.append(cp5)
+    patForRec.append(cp6)
+    patForRec.append(cp7)
+    patForRec.append(cp8)
+    patForRec.append(cp9)
+    patForRec.append(cp10)
+
+    print patForRec
+
+def patternRecognition():
+    for eachPattern in patternAr:
+        sim1 = 100.00 - abs(percentchange(eachPattern[0], patForRec[0]))
+        sim2 = 100.00 - abs(percentchange(eachPattern[1], patForRec[1]))
+        sim3 = 100.00 - abs(percentchange(eachPattern[2], patForRec[2]))
+        sim4 = 100.00 - abs(percentchange(eachPattern[3], patForRec[3]))
+        sim5 = 100.00 - abs(percentchange(eachPattern[4], patForRec[4]))
+        sim6 = 100.00 - abs(percentchange(eachPattern[5], patForRec[5]))
+        sim7 = 100.00 - abs(percentchange(eachPattern[6], patForRec[6]))
+        sim8 = 100.00 - abs(percentchange(eachPattern[7], patForRec[7]))
+        sim9 = 100.00 - abs(percentchange(eachPattern[8], patForRec[8]))
+        sim10 = 100.00 - abs(percentchange(eachPattern[9], patForRec[9]))
+
+        howSim = (sim1+sim2+sim3+sim4+sim5+sim6+sim7+sim8+sim9+sim10)/10.00
+
+        if howSim > 70:
+            patdex = patternAr.index(eachPattern)
+
+            print '##################################'
+            print '##################################'
+
+            print patForRec
+
+            print '=================================='
+            print '=================================='
+
+            print eachPattern
+            print '----------------------------------'
+            print 'predicted outcome', performanceAr[patdex]
+            xp = [1,2,3,4,5,6,7,8,9,10]
+            fig = plt.figure()
+            plt.plot(xp,patForRec)
+            plt.plot(xp, eachPattern)
+            plt.show()
+
+            print '##################################'
+            print '##################################'
+
+
 
 
 
@@ -61,6 +158,14 @@ def graphRawFX():
     plt.show()
 
 
+patternStorage()
+currentPattern()
+patternRecognition()
+totalTime = time.time() - totalStart
+print 'Entire processing time took:', totalTime, 'seconds'
 
-patternFinder()
+#currentPattern()
+#print percentchange(-10,10)
+#print patternAr[5]
+#patternStorage()
 #graphRawFX()
